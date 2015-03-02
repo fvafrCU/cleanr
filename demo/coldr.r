d263f0b76565fcd1243fdcf44c4281433d02a7f2
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2015-03-02 12:55:33 +0100

    add a demo() Kludge
    The demo doesn't work, but it's there and outputs a warning.

---
+++ demo/coldr.r
@@ -0,0 +1,5 @@
+#!/usr/bin/Rscript --vanilla
+# Rscript does not load methods, which is required by utils::package.skeleton()
+library(methods)
+library(coldr)
+warning('#FIXME: This is just a Kludge!')
