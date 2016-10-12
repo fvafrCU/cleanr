15193abd3bf006373f545f8d1abefbb4a681ccdd
foobar <none@nowhere.no>
2016-10-12 09:29:23 +0200

    make RUnit run

--- inst/tests/runit/directories.r
+++ inst/tests/runit/directories.r
@@ -1,5 +1,6 @@
 test_directories <- function() {
-    checkTrue(check_directory(system.file('source', 'R', package = 'cleanr'),
+    RUnit::checkTrue(check_directory(system.file("source", "R",
+                                                     package = "cleanr"),
                     max_arguments = 6, max_width = 100)
     )
 }
--- inst/tests/runit/files.r
+++ inst/tests/runit/files.r
@@ -1,11 +1,12 @@
 test_files <- function() {
-    checkTrue(check_file(system.file('source', 'R', 'checks.r',
-                                     package = 'cleanr'))
+    RUnit::checkTrue(check_file(system.file("source", "R", "checks.r",
+                                     package = "cleanr"))
     )
-    checkTrue(check_file(system.file('source', 'R', 'wrappers.r',
-                                     package = 'cleanr'), max_arguments = 6)
+    RUnit::checkTrue(check_file(system.file("source", "R", "wrappers.r",
+                                     package = "cleanr"), max_arguments = 6)
     )
-    checkException(check_file(system.file('source', 'R', 'wrappers.r',
-                                     package = 'cleanr'))
+    RUnit::checkException(check_file(system.file("source", "R",
+                                                     "wrappers.r",
+                                                     package = "cleanr"))
     )
 }
