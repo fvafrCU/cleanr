09ea567a6127c720831e0544fb182207f0b9fefa
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-10-13 17:59:03 +0200

    start an intro-vignette

---
+++ vignettes/cleanr_Introduction.R
@@ -0,0 +1,19 @@
+## ------------------------------------------------------------------------
+library("cleanr")
+set_cleanr_options(reset = TRUE)
+file_name <- system.file("source", "R", "utils.R", package = "cleanr")
+
+message("If a file passes all checks, cleanr returns invisibly TRUE")
+print(suppressWarnings(check_file(file_name)))
+
+message("Show current options:")
+get_cleanr_options(flatten_list = TRUE)
+
+message("Change an option to procude conditions:")
+set_cleanr_options(max_width = get_cleanr_options("max_width") - 1)
+
+message("Catch cleanr conditions:")
+print(tryCatch(suppressWarnings(check_file(file_name)),
+               cleanr = function(e) return(e)))
+
+
