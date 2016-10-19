df9f3dbb7dadd9ad5c2976ed1afbc75b4c9b026f
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-10-19 12:50:38 +0200

    add new unit tests

--- inst/tests/runit/functions.r
+++ inst/tests/runit/functions.r
@@ -5,3 +5,10 @@ test_internals <- function() {
     print(result)
     RUnit::checkIdentical(result, expectation)
 }
+test_functions_in_file <- function() {
+    RUnit::checkTrue(check_functions_in_file(system.file("source", "R", "checks.R",
+                                                         package = "cleanr")))
+    RUnit::checkException(check_functions_in_file(system.file("source", "R", "checks.R",
+                                                         package = "cleanr"),
+                                                  max_line_width = 10))
+}
