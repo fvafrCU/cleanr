213b7ff71745712e5531f12888bba39d31fad8b0
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-10-19 11:15:29 +0200

    fix error in test file
    I had an error implemented to catch in it in the make process...

--- inst/tests/runit/functions.r
+++ inst/tests/runit/functions.r
@@ -1,7 +1,7 @@
 test_internals <- function() {
     assign("f", function() print(3))
     result <- cleanr:::get_function_body(f)
-    expectation <- "print(2)"
+    expectation <- "print(3)"
     print(result)
     RUnit::checkIdentical(result, expectation)
 }
