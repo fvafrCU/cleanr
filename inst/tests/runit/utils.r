719a06159df06eb3a5712d409236d68f9eb8c484
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-10-24 13:08:14 +0200

    fix RUnit
    broke test ny trying to trick my way through lintr, I now use .lintr and the and
    exlusion tags. lintr has some issue
    (https://github.com/jimhester/lintr/issues/27).

--- inst/tests/runit/utils.r
+++ inst/tests/runit/utils.r
@@ -1,8 +1,6 @@
 test_options <- function() {
+    # Begin Exclude Linting
     assign("a", 3, envir = .GlobalEnv)
-    # trick lintr: the alternative would be to disable the object_usage_linter 
-    # (see https://github.com/jimhester/lintr/issues/27)
-    a <- 2 
     RUnit::checkTrue(is_not_false(a, envir = .GlobalEnv))
     RUnit::checkTrue(is_not_false(a, null_is_false = FALSE, envir = .GlobalEnv))
     rm("a", envir = .GlobalEnv)
@@ -12,4 +10,5 @@ test_options <- function() {
     assign("a", NULL, envir = .GlobalEnv)
     RUnit::checkTrue(! is_not_false(a, envir = .GlobalEnv))
     RUnit::checkTrue(is_not_false(a, null_is_false = FALSE, envir = .GlobalEnv))
+    # End Exclude Linting
 }
